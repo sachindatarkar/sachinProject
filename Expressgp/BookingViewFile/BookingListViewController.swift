f28d7d4d605a96d414395d4391e4dc6e5dcb9170
Sachin Datarkar <sachindatarkar90@gmail.com>
2019-05-08 21:01:30 +0530

    add booking list view

---
+++ Expressgp/BookingViewFile/BookingListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  BookingListViewController.swift
+//  Expressgp
+//
+//  Created by Sachin on 07/05/19.
+//  Copyright © 2019 Expressgp. All rights reserved.
+//
+
+import UIKit
+
+class BookingListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
